66b3a35ac4f9e565a6850eb7cef1ec6c324a9abf
yychennie <yychennie@126.com>
2017-06-04 19:37:38 +0800

    change4

---
+++ tlpro/tlpro/Httpadd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tlpro
+{
+    class Httpadd
+    {
+        private static String add = "192.168.191.1";
+        public static String Add
+        {
+            get { return add; }
+            set { add = value; }
+        }
+    }
+}
