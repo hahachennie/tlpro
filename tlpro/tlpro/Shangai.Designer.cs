e20345a1add3b182c7075b5359524496d0822976
yychennie <yychennie@126.com>
2017-06-04 19:54:52 +0800

    change10

--- tlpro/tlpro/Shangai.Designer.cs
+++ tlpro/tlpro/Shangai.Designer.cs
@@ -65,6 +65,7 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "确认修改";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
