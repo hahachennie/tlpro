f00444e38ecc890c0dc2bfcd5d1ca4d895182c1a
yychennie <yychennie@126.com>
2017-06-04 19:50:33 +0800

    addti

--- tlpro/tlpro/Addti.Designer.cs
+++ tlpro/tlpro/Addti.Designer.cs
@@ -69,6 +69,7 @@
             this.button4.Text = "删除此题";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
