66ca840a382976a986e97ce27b1d0dc04abb7d6f
yychennie <yychennie@126.com>
2017-06-04 19:45:30 +0800

    stumain

--- tlpro/tlpro/StuMain.Designer.cs
+++ tlpro/tlpro/StuMain.Designer.cs
@@ -102,6 +102,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "出题";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // radioButton2
             // 
